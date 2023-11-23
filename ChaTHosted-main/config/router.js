ee83f69918eb806b2e95f26851ad546793153d45
Cano's Code  (；●∀●)ゝ” <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2023-11-23 17:05:16 -0600

    Delete ChaTHosted-main directory

--- ChaTHosted-main/config/router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const {pathname:root} = new URL("..", import.meta.url);
-
-const rout = express.Router();
-rout.get("/", (req,res)=>{
-    res.sendFile("/public/chat.html",{root:"."})
-})
-
-export const router = rout;
